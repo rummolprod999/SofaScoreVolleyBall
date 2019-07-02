03d68790a05942c03442b0a6a64f13ff945e13a5
rummolprod999 <rummolprod999@gmail.com>
2019-07-02 10:13:32 +0300

    fix issue

--- SofaVolleyBall.go
+++ SofaVolleyBall.go
@@ -17,6 +17,7 @@ func (t *SofaVolleyBall) run() {
 }
 
 func (t *SofaVolleyBall) workWithResponse(s string) {
+	defer SaveStack()
 	_, err := jsonparser.ArrayEach([]byte(s), func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
 		if err != nil {
 			Logging(err)
@@ -28,15 +29,43 @@ func (t *SofaVolleyBall) workWithResponse(s string) {
 			Logging(err)
 			return
 		}
-		jsonparser.ArrayEach([]byte(scoring), func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-			val, _, _, _ := jsonparser.Get(value, "homeScore")
-			fmt.Println(string(val))
-		})
-
+		_, err = jsonparser.ArrayEach([]byte(scoring), t.VolleyBallMatch)
+		if err != nil {
+			Logging(err)
+			return
+		}
 	}, "sportItem", "tournaments")
 	if err != nil {
 		Logging(err)
 		return
 	}
 
+}
+
+func (t *SofaVolleyBall) VolleyBallMatch(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+	homeTeam, _, _, err := jsonparser.Get(value, "homeTeam", "name")
+	if err != nil {
+		Logging(err)
+		return
+	}
+	homeScore, _, _, err := jsonparser.Get(value, "homeScore")
+	if err != nil {
+		Logging(err)
+		return
+	}
+	awayTeam, _, _, err := jsonparser.Get(value, "awayTeam", "name")
+	if err != nil {
+		Logging(err)
+		return
+	}
+	awayScore, _, _, err := jsonparser.Get(value, "awayScore")
+	if err != nil {
+		Logging(err)
+		return
+	}
+	fmt.Println(string(homeTeam))
+	fmt.Println(string(homeScore))
+	fmt.Println(string(awayTeam))
+	fmt.Println(string(awayScore))
+	fmt.Println()
 }
