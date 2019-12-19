cf4079615939679588598dff58a271b483ae28da
rummolprod999 <rummolprod999@gmail.com>
2019-12-19 15:47:50 +0300

    fix issue

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
+import "time"
+
 func init() {
 	CreateEnv()
 }
 
 func main() {
 	defer SaveStack()
+	currentTime := time.Now()
+	if currentTime.Hour() > 19 || currentTime.Hour() < 12 {
+		return
+	}
 	server := SofaVolleyBall{}
 	server.run()
 }
